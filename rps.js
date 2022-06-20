8cdda2892e5b13058c2d42cff21b79b998a5168c
Jake <jakenead@gmail.com>
2022-06-19 23:04:06 -0700

    Create first draft of playRound

--- rps.js
+++ rps.js
@@ -1,7 +1,5 @@
 
-const choice = ['rock', 'paper', 'scissors'];
 const randomNumber = Math.floor(Math.random()*3);
-
 
 function computerPlay () {
     if (randomNumber == 0) {
@@ -12,3 +10,32 @@ function computerPlay () {
         return 'scissors';
     } 
     console.log(computerPlay())
+
+const humanPlay = 'rock';
+const win = 'You totally won!';
+const lose = 'Bummer you lost...';
+const tie = 'You tied! Try again!';
+
+
+function playRound(humanPlay, computerPlay) {
+    if (humanPlay == computerPlay) {
+        return tie;
+    } else if (humanPlay = 'rock' && computerPlay == 'paper' || 
+    humanPlay == 'paper' && computerPlay == 'scissors' || 
+    humanPlay =='scissors' && computerPlay == 'rock') {
+        return lose;
+    } else {
+        return win;
+    }
+}
+
+console.log(playRound())
+// console.log(playRound('paper', 'scissors'))
+
+
+
+//Unused code below here.
+
+// const choice = ['rock', 'paper', 'scissors'];
+
+// if ('rock', 'rock' || 'scissors', 'scissors' || 'paper', 'paper') 
