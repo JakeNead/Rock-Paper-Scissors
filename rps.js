355c3ce3441aa0b6eb6707aaf632f7336fe52ab2
Jake <jakenead@gmail.com>
2022-06-19 22:12:41 -0700

    Create functional computerPlay function

--- rps.js
+++ rps.js
@@ -1,15 +1,14 @@
+
+const choice = ['rock', 'paper', 'scissors'];
+const randomNumber = Math.floor(Math.random()*3);
+
+
 function computerPlay () {
-    const action = ['rock', 'paper', 'scissors'];
-    const randomNumber = Math.floor(Math.random() * action.length);
     if (randomNumber == 0) {
         return 'rock';
-    }else if (randomNumber == 1) {
+    } else if (randomNumber == 1) {
         return 'paper';
-    } else if (randomNumber == 2) {
+    } else  (randomNumber == 2) 
         return 'scissors';
-    }
-    }
-// const random = Math.floor(Math.random() *3)
- 
-
-// console.log(random)
+    } 
+    console.log(computerPlay())
