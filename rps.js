454c3b502f1afd5f72f7f01f5b1a89b3f549747d
Jake Nead <jakenead@gmail.com>
2022-10-17 18:48:26 -0700

    Use buttons as game input instead of a prompts

--- rps.js
+++ rps.js
@@ -1,10 +1,8 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function game() {
-    let humanPlay = prompt('Choose rock, paper or scissors').toLowerCase()
+function game(humanPlay) {
     let computerPlay = ''
-
     function computerSelection () {
         let randomNumber = Math.floor(Math.random()*3);
             if (randomNumber === 0) {
@@ -18,13 +16,16 @@ function game() {
 
     function win () {
         humanScore += 1;
+        result.textContent = "Human won the round!"
         console.log('You won!');
     }
     function lose () {
-        computerScore += 1;
+        computerScore += 1
+        result.textContent = "Computer won the round!"
         console.log('You lost...');
     }
     function tie () {
+        result.textContent = "Tie!"
         console.log('You tied!');
     }
 
@@ -35,26 +36,44 @@ function game() {
     else if (humanPlay === 'rock' && computerPlay === 'paper' || 
         humanPlay === 'paper' && computerPlay === 'scissors' || 
         humanPlay ==='scissors' && computerPlay === 'rock') {;
-    return lose();
+        return lose();
     } else if (humanPlay === computerPlay) {
-    return tie();
+        return tie();
     } else {
-    return win();
+        return win();
     }
-
 }      
-   
-for (let i = 0; i < 5; i++) {
-    if (i < 4) {
-        game();
-    } else {
-        if (computerScore > humanScore) {
-            console.log('The computer wins the day!')
-        } else if (computerScore < humanScore) {
-            console.log('The Human won the day!')
-        } else  { 
-            console.log('It\'s a tie game. Try again!');
-        }
-        console.log(`Computer ${computerScore}, Human ${humanScore}`)
-    }
-}
+
+const rock = document.querySelector('.rock')
+rock.addEventListener('click', () => {
+    game('rock')
+})
+
+const paper = document.querySelector('.paper')
+paper.addEventListener('click', () => {
+    game('paper')
+})
+
+const scissors = document.querySelector('.scissors')
+scissors.addEventListener('click', () => {
+    game('scissors')
+})
+
+
+const score = document.querySelector('score')
+
+
+// for (let i = 0; i < 5; i++) {
+//     if (i < 4) {
+//         game();
+//     } else {
+//         if (computerScore > humanScore) {
+//             console.log('The computer wins the day!')
+//         } else if (computerScore < humanScore) {
+//             console.log('The Human won the day!')
+//         } else  { 
+//             console.log('It\'s a tie game. Try again!');
+//         }
+//         console.log(`Computer ${computerScore}, Human ${humanScore}`)
+//     }
+// }
